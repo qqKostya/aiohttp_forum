498c43a3db976ac280816b76e1f0ca34c25a3b9f
qqKazakov <leap74rus@gmail.com>
2023-06-15 08:10:23 +0300

    add: models and etc.

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
-from aiohttp import web  # основной модуль aiohttp
-import jinja2  # шаблонизатор jinja2
-import aiohttp_jinja2  # адаптация jinja2 к aiohttp
+from app.settings import config, BASE_DIR
+from aiohttp import web
+import aiohttp_jinja2
+import jinja2
+
+from app.store.database.models import database_accessor
 
 
-# в этой функции производится настройка url-путей для всего приложения
+def setup_config(application):
+    application["config"] = config
+
+
+def setup_accessors(application):
+    database_accessor.setup(application)
+
+
+def setup_external_libraries(application):
+    aiohttp_jinja2.setup(
+        application, loader=jinja2.FileSystemLoader(f"{BASE_DIR}/templates")
+    )
+
+
 def setup_routes(application):
     from app.forum.routes import setup_routes as setup_forum_routes
 
-    setup_forum_routes(application)  # настраиваем url-пути приложения forum
-
-
-def setup_external_libraries(application: web.Application) -> None:
-    # указываем шаблонизатору, что html-шаблоны надо искать в папке templates
-    aiohttp_jinja2.setup(application, loader=jinja2.FileSystemLoader("templates"))
+    setup_forum_routes(application)
 
 
 def setup_app(application):
-    # настройка всего приложения состоит из:
-    setup_external_libraries(
-        application
-    )  # настройки внешних библиотек, например шаблонизатора
-    setup_routes(application)  # настройки роутера приложения
+    setup_config(application)
+    setup_accessors(application)
+    setup_external_libraries(application)
+    setup_routes(application)
 
 
-app = web.Application()  # создаем наш веб-сервер
+app = web.Application()
 
-if (
-    __name__ == "__main__"
-):  # эта строчка указывает, что данный файл можно запустить как скрипт
-    setup_app(app)  # настраиваем приложение
-    web.run_app(app)  # запускаем приложение
+if __name__ == "__main__":
+    setup_app(app)
+    web.run_app(app, port=config["common"]["port"])
